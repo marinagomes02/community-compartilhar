c4e0e4cb35e531bd6f9d1bacfe98c5f4da948008
João Nogueira <jrnogueira@edu.ulisboa.pt>
2024-01-25 17:25:57 +0000

    Implement landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,5 @@
-<div class="container">sup.</div>
+<div class="container mx-auto mb-20 mt-40 flex flex-col items-center justify-center">
+	<div class="h-48 w-48 rounded-full bg-foreground"></div>
+	<h1 class="mb-2 text-9xl font-medium">community</h1>
+	<p class="text-3xl text-muted-foreground">One of us, one of us...</p>
+</div>
