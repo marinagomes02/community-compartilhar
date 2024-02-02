69b477da5ced4d9e881935e03780d3b29535f1e9
João Nogueira <jrnogueira@edu.ulisboa.pt>
2024-02-02 01:41:23 +0000

    Implement events feature WIP

---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import PageHeader from '$lib/components/page-header.svelte';
+	import type { Event } from '$lib/types';
+	import EventItem from './_components/event-item.svelte';
+
+	const events: Event[] = [
+		{
+			id: '1',
+			title: 'How to make a cup of tea',
+			description: 'A simple guide to making a cup of tea',
+			image: '/images/how-to/1/1.jpg',
+			date: '2021-10-10',
+		},
+		{
+			id: '1',
+			title: 'How to make a cup of tea',
+			description: 'A simple guide to making a cup of tea',
+			image: '/images/how-to/1/1.jpg',
+			date: '2021-10-10',
+		},
+		{
+			id: '1',
+			title: 'How to make a cup of tea',
+			description: 'A simple guide to making a cup of tea',
+			image: '/images/how-to/1/1.jpg',
+			date: '2021-10-10',
+		},
+	];
+</script>
+
+<PageHeader title="Events" subtitle="Find & share events" />
+
+<div class="container flex flex-col gap-y-5">
+	{#each events as event}
+		<EventItem {event}></EventItem>
+	{/each}
+</div>
---
+++ src/routes/events/_components/event-item.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Card } from '$lib/components/ui/card';
+	import type { Event } from '$lib/types';
+
+	export let event: Event;
+</script>
+
+<Card class="h-10 w-full">.</Card>
