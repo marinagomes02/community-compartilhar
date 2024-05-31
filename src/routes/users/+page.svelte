55302c5efea37fbc5597b0bb2bf0e228836efab0
Marina Gomes <marina.gomes02@gmail.com>
2024-05-31 12:56:06 +0100

    [Members] Add search by region

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -2,23 +2,34 @@
 	import PageHeader from "@/components/page-header.svelte";
 	import type { PageData } from "./$types";
 	import ProfilePreview from "./_components/profile-preview.svelte";
-	import { CloseButton } from "flowbite-svelte";
 	import { Input } from "@/components/ui/input";
 
     export let data: PageData
 
-    let searchTerm = '';
-    $: filteredProfiles = searchTerm === '' ? data.profiles : data.profiles.filter(profile => profile.display_name.toLowerCase().includes(searchTerm.toLowerCase()));
+    let nameSearchTerm = '';
+    let regionSearchTerm = '';
+    $: filteredProfiles = nameSearchTerm === '' && regionSearchTerm === '' 
+                            ? data.profiles 
+                            : nameSearchTerm !== '' && regionSearchTerm !== '' 
+                                ? data.profiles.filter(profile => 
+                                    profile.display_name.toLowerCase().includes(nameSearchTerm.toLowerCase()) 
+                                    && profile.region.toLowerCase().includes(regionSearchTerm.toLowerCase()))
+                                : nameSearchTerm !== '' && regionSearchTerm === ''
+                                    ? data.profiles.filter(profile => profile.display_name.toLowerCase().includes(nameSearchTerm.toLowerCase()))
+                                    : data.profiles.filter(profile => profile.region.includes(regionSearchTerm.toLowerCase()))
 </script>
 
 <PageHeader title="Membros" subtitle="Conhece a tua comunidade" />
 <div class="p-10">
-    <div class="container flex flex-row space-x-3">
+    <div class="container flex flex-row space-x-6">
         <Input 
-            placeholder="⌕ Procurar..." 
-            bind:value={searchTerm}
-            class="w-80 mb-10">
-        </Input>
+            placeholder="Procurar por nome..." 
+            bind:value={nameSearchTerm}
+            class="w-80 mb-10" />
+        <Input
+            placeholder="Procurar por região..."
+            bind:value={regionSearchTerm}
+            class="w-80 mb-10"/>
     </div>
     <div class="container mx-auto grid grid-cols gap-6">
         {#each filteredProfiles as profile}
