7b0da2d270e10fd299dc4888cc188341049ce1bc
Marina Gomes <marina.gomes02@gmail.com>
2024-07-13 16:06:15 +0100

    [group] Add translation

--- src/routes/groups/+page.svelte
+++ src/routes/groups/+page.svelte
@@ -23,23 +23,10 @@
 
 <PageHeader title="Grupos" subtitle="Regista o teu grupo de patrocínio" />
 <div class="container pb-10">
-    <Modal 
-        openModal={openSearchModal} 
-        header="Procurar grupo de patrocínio"
-        accept="Continuar"
-        decline="Cancelar"
-        redirectOnAccept="groups/join/create"
-        >  
-        <p class="mb-3"> Quer ser patrocinador mas não tem grupo? Aqui, pode fazer um pedido para se juntar a outros num grupo.</p>
-        <p class="text-black-500 mb-1"><strong>Informação:</strong> 
-        <p class="flex flex-row pl-2 mb-1"><Dot class="mr-2 strong-icon w-fit"/> Para fazer parte de um grupo, terá que preencher o seguinte formulário. Quando houver outros participantes disponíveis, a plataforma criará o grupo, que deverá ser aprovado pelo CPR.</p>   
-        <p class="flex flex-row pl-2 mb-1"><Dot class="mr-2 strong-icon w-fit"/> Para formar um grupo equilibrado, é necessário reunir todas as funções necessárias para o patrocínio, pelo que será agrupado com candidatos com funcções complementares</p>
-        <p class="flex flex-row pl-2 mb-1"><Dot class="mr-2 strong-icon w-fit"/> Apenas serão procuradas pessoas dentro da distância que definir, para garantir apoio presencial à família de refugiados. Certifique-se de ter definido o seu pin no mapa da plataforma.</p>
-    </Modal>
     <Tabs.Root value="look-for-group">
-        <Tabs.List>
-            <Tabs.Trigger value="register">{translate(locale, "general")}</Tabs.Trigger>
-			<Tabs.Trigger value="look-for-group">{translate(locale, "groupsModeration.title")}</Tabs.Trigger>
+        <Tabs.List class="bg-yellow-200">
+            <Tabs.Trigger value="register">{translate(locale, "registerGroup")}</Tabs.Trigger>
+			<Tabs.Trigger value="look-for-group">{translate(locale, "searchGroup")}</Tabs.Trigger>
 		</Tabs.List>
 		<Tabs.Content value="register">
             <Card.Root>
@@ -66,8 +53,8 @@
                 </Card.Header>
                 <Card.Content>
                     <div class="w-full justify-center items-center">
-                        <p class="mt-4 mb-2">{translate(locale, "createGroupModal.inst1")}</p>
-                        <div class="w-full flex flex-col pl-2 space-y-1"> 
+                        <p class="mt-4 mb-3">{translate(locale, "createGroupModal.inst1")}</p>
+                        <div class="w-full flex flex-col pl-5 space-y-1"> 
                             <div class="flex flex-row items-center space-x-2">
                                 <CircleCheck size="16" color="rgb(42,157, 143)"/> 
                                 <span>{translate(locale, "createGroupModal.inst2")}</span>
@@ -99,21 +86,21 @@
 		<Tabs.Content value="look-for-group">
             <Card.Root>
                 <Card.Header>
-                    <Card.Title>Nearby users looking for a group</Card.Title>
+                    <Card.Title>{translate(locale, "nearbyUsersLookingForGroup")}</Card.Title>
                     <Card.Description>
                         {#if data.near_by_users.length > 0}
                             <div class="mt-1" >
-                                <p>You have <strong>{data.near_by_users.length} users</strong>  near your location that are looking for a new sposorship group.</p>
-                                <p>Meet them below and create a sponsorship group to help another refugee family! </p>
+                                <p>{translate(locale, "youHave")} <strong>{data.near_by_users.length} {translate(locale, "people")}</strong> {translate(locale, "nearYourLocation")}</p>
+                                <p>{translate(locale, "meetThem")}</p>
                             </div>
                             {:else}
-                            <p class="mt-1">There are no users near your location looking for a new sponsorship group, at the moment.</p>
+                            <p class="mt-1">{translate(locale, "noUsersNearby")}</p>
                             {/if}
                         </Card.Description>
                     </Card.Header>
                     <Card.Content> 
                         {#if data.near_by_users.length > 0}
-                            <h5 class="mb-2"> People near you: </h5>
+                            <h5 class="mb-2"> {translate(locale, "peopleNearYou")} </h5>
                             <div class="grid grid-cols-2 w-fit items-center gap-x-7 row-border">
                                 {#each data.near_by_users as user}
                                     <div class="w-fit flex flex-row items-center p-2 space-x-3">
