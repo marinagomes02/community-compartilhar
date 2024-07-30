aa78146fe3155c89cd9dcae2c1c80fbebbcebea1
Marina Gomes <marina.gomes02@gmail.com>
2024-07-30 23:48:14 +0100

    [Tutorials] logs for fixing

--- src/routes/academy/+layout.server.ts
+++ src/routes/academy/+layout.server.ts
@@ -2,6 +2,7 @@ import type { LayoutServerLoad } from '../$types';
 
 export const load: LayoutServerLoad = async ({ parent }) => {
 	const parentData = await parent();
+	console.log("locale on academy/+layout.server.ts:", parentData.languagePreference)
 	return {
 		locale: parentData.languagePreference,
 	};
